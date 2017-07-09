2f11a7be1eaa44d196978d36713f628cf2ffe405
Amr <12211826+416d72@users.noreply.github.com>
2017-07-09 02:26:46 +0200

    Version 0.7
    Former-commit-id: 1053f18691ee73b49ba0a82e1d68e50315e2e967
    Former-commit-id: 81e6a645c21649e70a39f5d9a21d794ad7c32684
    Former-commit-id: 1a250dabb306c7c60ebd56df04f33b4996084fa0

---
+++ App/Views/blog/about.php
@@ -0,0 +1,24 @@
+<br>
+<div id="main-content" class="col-sm-9 col-xs-12">
+    <div class="box wow post-box">
+        <div class="post-content">
+            <h1 class="heading">About</h1>
+            <br>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas urna id quam tristique blandit. Donec ipsum orci, ultrices eu elementum sed, consequat eget mi. In nec vulputate felis, at blandit purus. Vestibulum vel mollis orci. Curabitur ut cursus mauris. Morbi congue varius aliquam. Cras massa nulla, malesuada quis consequat nec, facilisis nec tortor. Nulla libero lectus, viverra sodales hendrerit nec, ullamcorper ut dui. Morbi bibendum vehicula libero, sed suscipit dolor aliquet at. Nullam posuere feugiat pharetra. Maecenas quis feugiat sem. Suspendisse eget enim nec mauris congue posuere.
+            </p>
+            <p>
+                Nullam facilisis nunc eu elementum vehicula. Mauris porttitor bibendum tincidunt. Cras eget libero purus. Integer venenatis elit ac nunc rhoncus placerat ut tempus nunc. Sed euismod arcu ut leo viverra, vel consequat lorem aliquet. Mauris ornare ante massa, in placerat lacus interdum sit amet. Duis eget vestibulum ipsum, sed tristique justo. Aliquam urna tortor, varius eu odio et, commodo scelerisque arcu. Fusce cursus lectus viverra imperdiet sodales.
+            </p>
+            <p>
+                Pellentesque eu lacus arcu. Suspendisse venenatis, mi in vulputate convallis, ex neque ornare nisi, at dictum justo neque nec diam. Quisque quis felis eu urna consectetur ornare id sed ligula. Quisque et purus dictum, interdum augue eleifend, molestie massa. Ut convallis urna ullamcorper mi dignissim, eu malesuada augue dapibus. Praesent felis eros, ultrices quis bibendum quis, consectetur sit amet justo. Aenean vulputate gravida volutpat. Vivamus pharetra luctus rhoncus. Sed maximus viverra sagittis. Nulla felis nibh, facilisis a mi sed, laoreet lobortis lectus. Maecenas sed aliquam mi.
+            </p>
+            <p>
+                Curabitur quis porta leo. Vivamus risus ex, posuere vel sodales nec, accumsan pretium purus. Fusce dapibus venenatis imperdiet. Maecenas porttitor nulla ut posuere sodales. Duis posuere ultrices imperdiet. Suspendisse ut dapibus leo, eget euismod nibh. Morbi ultricies mi eros, nec rutrum ipsum luctus et.
+            </p>
+            <p>
+                Aenean pellentesque libero erat, in volutpat arcu sodales non. Suspendisse et sem imperdiet, porttitor erat ac, imperdiet mauris. Sed malesuada sem augue, nec posuere nulla fringilla ac. Pellentesque egestas sapien sit amet nulla mollis ultricies. Nullam at bibendum arcu. Quisque enim turpis, elementum eu hendrerit sed, ultricies ut elit. Mauris quis sapien risus. Suspendisse potenti.
+            </p>
+        </div>
+    </div>
+</div>
