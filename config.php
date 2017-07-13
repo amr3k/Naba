e53f257d73d92b602534e01bed4444f9ff26da9a
Amr <12211826+416d72@users.noreply.github.com>
2017-07-13 20:07:05 +0200

    Enhanced security against XSS attacks
    Former-commit-id: 499ade3bbacb7e5df491b26a88a4d421024ab4f5
    Former-commit-id: bff7ab1050f0b042f1b91ae8c9f8959fe688a01a
    Former-commit-id: 2779a5fb0c62133f1d600ee4fb6a217ca3604b96

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
     'server' => 'localhost',
-    'dbname' => 'id2210053_logo',
-    'user'   => 'id2210053_root',
-    'pass'   => '4.5.6.3.2.1.8.5.2.'
+    'dbname' => 'blog',
+    'user'   => 'root',
+    'pass'   => ''
 ];
