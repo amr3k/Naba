2eb2b4e8eea1686676126a9b5ec8f5f9ee9f2986
Amr <akkk33@protonmail.com>
2017-07-15 09:17:10 +0200

    Tiny UI enhancements
    Former-commit-id: ba253c7c7b52cf3f8b1fd40dff29494a3887c9bf
    Former-commit-id: b260c3810e8e46d32f085621d113a706d1f04dc7
    Former-commit-id: cfc1c20167c1cf65ab054c43611f296b723fdb36

--- App/Controllers/Admin/PostsController.php
+++ App/Controllers/Admin/PostsController.php
@@ -134,8 +134,8 @@ class PostsController extends Controller
                 ->max('title', 70, 'Title cannot be more than 70 characters');
         $this->validator
                 ->required('text', 'Please type some text')
-                ->min('text', 200, 'Title cannot be less than 200 characters')
-                ->max('text', 10000, 'Title cannot be more than 10000 characters');
+                ->min('text', 200, 'Body cannot be less than 200 characters')
+                ->max('text', 10000, 'Body cannot be more than 10000 characters');
         $this->validator->required('tags', 'Please submit some tags')->min('tags', 3)->max('tags', 33);
         if (is_null($id)) {
             // If the $id is null,
