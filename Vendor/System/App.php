caa616ac48446ea793f729a5e8e975b1f245162f
Amr <akkk33@protonmail.com>
2017-11-14 23:27:21 +0200

    Former-commit-id: 44f67268192f69ca9321e36a133c140f4779816a Former-commit-id: 6f0ae5cc36414e136f0d23b8b4220a03cc9f2145 Former-commit-id: bf45191197d1cb0dc4e62e62875d36c3fdf7762d

--- Vendor/System/App.php
+++ Vendor/System/App.php
@@ -67,6 +67,7 @@ class App
         $this->session->start();
         $this->request->prepareUrl();
         $this->file->call('App/index.php');
+        $this->file->call('Vendor/autoload.php');
         list($controller, $method, $arguments) = $this->route->getProperRoute();
         if ($this->route->hasCallsFirst()) {
             $this->route->callFirstCalls();
