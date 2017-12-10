60a39e02926f0bc9ade08d7d8428460a5e6338fe
Amr <akkk33@protonmail.com>
2017-12-10 15:13:48 +0200

    Minor improvements
    Former-commit-id: a3dc024f73e65c23c792738595e6de3c6b02db24
    Former-commit-id: 31b79c536b92599445ac36be1c88be31744b2ecf
    Former-commit-id: 5fadc66832ea6ac2c0aa651e7eb5597116489601

--- public/blog/js/custom.js
+++ public/blog/js/custom.js
@@ -1,30 +1,3 @@
-// Changing some meta tags in order to enhance sharing :
-var FBappID = document.getElementById('FBappID').content
-        , postDesc = document.getElementById('postDesc').innerHTML
-        , postImg = document.getElementById('postImg').src;
-document.getElementById('metaPostDesc').content = postDesc;
-document.getElementById('metaPostImg').content = postImg;
-// Facebook Javascript SDK
-window.fbAsyncInit = function () {
-    FB.init({
-        appId: FBappID,
-        autoLogAppEvents: true,
-        xfbml: true,
-        version: 'v2.11'
-    });
-};
-
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=126734348075212";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 $(function () {
     adjustFooter();
 
@@ -157,3 +130,30 @@ function setFixedFooter() {
         }
     });
 }
+// Changing some facebook meta tags :
+var FBappID = document.getElementById('FBappID').content
+        , postDesc = document.getElementById('postDesc').innerHTML
+        , postImg = document.getElementById('postImg').src;
+document.getElementById('metaPostDesc').content = postDesc;
+document.getElementById('metaPostImg').content = postImg;
+// Facebook Javascript SDK
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: FBappID,
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: 'v2.11'
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=126734348075212";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
