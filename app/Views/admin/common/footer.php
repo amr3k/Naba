489ae5561f109b0b44e74fce690c1de07b1a8cde
Amr <12211826+416d72@users.noreply.github.com>
2017-12-10 03:36:11 +0200

    Minor fixes
    Former-commit-id: fa195b048666de84853c26e25ea1b58e08d73a7a
    Former-commit-id: 5927d864ec6ef8f181115f2bf6ee28d4d7e47adf
    Former-commit-id: 2b533fef7408eaf43341aa7743ea9b020df35581

--- app/Views/admin/common/footer.php
+++ app/Views/admin/common/footer.php
@@ -11,7 +11,7 @@
 <!-- jQuery 2.2.0 -->
 <script src="<?php echo assets('admin/plugins/jQuery/jQuery-2.2.0.min.js'); ?>"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+<script src="<?php echo assets('assets/js/jquery-ui.min.js'); ?>"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
     $.widget.bridge('uibutton', $.ui.button);
@@ -19,7 +19,7 @@
 <!-- Bootstrap 3.3.6 -->
 <script src="<?php echo assets('admin/bootstrap/js/bootstrap.min.js'); ?>"></script>
 <!-- Morris.js charts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="<?php echo assets('assets/js/raphael-min.js'); ?>"></script>
 <script src="<?php echo assets('admin/plugins/morris/morris.min.js'); ?>"></script>
 <!-- Sparkline -->
 <script src="<?php echo assets('admin/plugins/sparkline/jquery.sparkline.min.js'); ?>"></script>
@@ -29,7 +29,7 @@
 <!-- jQuery Knob Chart -->
 <script src="<?php echo assets('admin/plugins/knob/jquery.knob.js'); ?>"></script>
 <!-- daterangepicker -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+<script src="<?php echo assets('assets/js/moment.min.js'); ?>"></script>
 <script src="<?php echo assets('admin/plugins/daterangepicker/daterangepicker.js'); ?>"></script>
 <!-- datepicker -->
 <script src="<?php echo assets('admin/plugins/datepicker/bootstrap-datepicker.js'); ?>"></script>
