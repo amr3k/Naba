ac5cd157e650604ac4fffe33c1804c62aa82b53d
Amr <akkk33@protonmail.com>
2017-12-11 02:09:04 +0200

    Small fixes
    Former-commit-id: 5f3d143750c6d7b437dce9cd7063ed4627d9ba76
    Former-commit-id: 7089a36b0db65a9ab021bd34973607bb0d35fe90
    Former-commit-id: ecbe3da5f7b2689dd1b9d7ea19781ab572960897

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require __DIR__ . '/vendor/System/App.php';
 require __DIR__ . '/vendor/System/File.php';
 
 use System\File;
-use System\App;
+use System\app;
 
 $file = new File(__DIR__);
-$app  = App::getInstance($file);
+$app  = app::getInstance($file);
 $app->run();
