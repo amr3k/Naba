2864a409084dc0a51fe65baa2d151e490b5b611c
Amr <akkk33@protonmail.com>
2018-01-20 13:21:45 +0200

    Minor fixes
    Former-commit-id: 5f9b6966c1bc20b6a54a5007e8c926b9b2539a5a
    Former-commit-id: d8cb01a87ea56a36e67fa97744b681d655e36e87
    Former-commit-id: 874555f87687b0f1eca99b5c9a5e0412cff86fcd

--- app/Views/admin/common/sidebar.php
+++ app/Views/admin/common/sidebar.php
@@ -10,38 +10,38 @@
                 </a>
             </li>
             <li id="sidebar-categories" class="sidebar-link">
-                <a href="<?php echo url('/admin/categories') ?>">
+                <a href="<?php echo url('/admin/categories'); ?>">
                     <i class="fa fa-list"></i>
                     <span>Categories</span>
                 </a>
             </li>
             <li id="sidebar-users-groups" class="sidebar-link">
-                <a href="<?php echo url('/admin/users-groups') ?>">
+                <a href="<?php echo url('/admin/users-groups'); ?>">
                     <i class="fa fa-users"></i>
                     <span>Users-Groups</span>
                 </a>
             </li>
             <li id="sidebar-users" class="sidebar-link">
-                <a href="<?php echo url('/admin/users') ?>">
+                <a href="<?php echo url('/admin/users'); ?>">
                     <i class="fa fa-user"></i>
                     <span>Users</span>
                 </a>
             </li>
             <li id="sidebar-posts" class="sidebar-link">
-                <a href="<?php echo url('/admin/posts') ?>">
+                <a href="<?php echo url('/admin/posts'); ?>">
                     <i class="fa fa-paper-plane-o"></i>
                     <span>Posts</span>
                 </a>
             </li>
             <li id="sidebar-comments" class="sidebar-link">
-                <a href="<?php echo url('/admin/comments') ?>">
+                <a href="<?php echo url('/admin/comments'); ?>">
                     <i class="fa fa-comment" aria-hidden="true"></i>
                     <span>Comments</span>
                 </a>
             </li>
             <li id="sidebar-ads" class="sidebar-link">
-                <a href="<?php echo url('/admin/ads') ?>">
-                    <i class="fa fa-flag"></i>
+                <a href="<?php echo url('/admin/ads'); ?>">
+                    <i class="fa fa-flag" aria-hidden="true"></i>
                     <span>Ads</span>
                 </a>
             </li>
