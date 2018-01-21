ca716e89a112ffe38a7b461d040546a2715c8f2b
Amr <akkk33@protonmail.com>
2018-01-21 16:44:31 +0200

    Minor fixes
    Former-commit-id: ca53623efce9e1986a00c3c011601bae84d1ee3a
    Former-commit-id: 05fc25562201def5058d818aaf8b61cc8414598e
    Former-commit-id: 833f4608cde262253a7727cba7b238cb8faafad9

--- app/Controllers/Blog/Common/SidebarController.php
+++ app/Controllers/Blog/Common/SidebarController.php
@@ -12,6 +12,7 @@ class SidebarController extends Controller
         $data['categories'] = $this->load->model('Categories')->getEnabledCategoriesWithNumberOfTotalPosts();
         $data['posts']      = $this->load->model('Posts')->getPostsByViews();
         $data['ads']        = $this->load->model('Ads')->enabled();
+        $data['status']     = $this->load->model('Settings')->all()[2]->v;
         return $this->view->render('blog/common/sidebar', $data);
     }
 
